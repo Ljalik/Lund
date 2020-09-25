8f7cbe7efa279c383adafbac762b96a467004661
Opilane TTHK <opilane@tthk.ee>
2020-09-25 12:22:59 +0300

    Add project files.

---
+++ Lund/Lund/Lund.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Lund
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Lund : ContentPage
+    {
+        BoxView pea, amber, keha;
+        public Lund()
+        {
+            AbsoluteLayout abs = new AbsoluteLayout();
+            //голова
+            pea = new BoxView { Color = Color.LightBlue, CornerRadius = 90 };
+            AbsoluteLayout.SetLayoutBounds(pea, new Rectangle(0.3, 0.2, 200, 200));
+            AbsoluteLayout.SetLayoutFlags(pea, AbsoluteLayoutFlags.All);
+            abs.Children.Add(pea);
+            // тело
+            keha = new BoxView { Color = Color.Red, CornerRadius = 90 };
+            AbsoluteLayout.SetLayoutBounds(keha, new Rectangle(0.5, 0.5, 300, 300));
+            AbsoluteLayout.SetLayoutFlags(keha, AbsoluteLayoutFlags.All);
+            abs.Children.Add(keha);
+            // ведро
+            amber = new BoxView { Color = Color.DarkKhaki };
+            AbsoluteLayout.SetLayoutBounds(amber, new Rectangle(0.2, 0.1, 100, 100));
+            AbsoluteLayout.SetLayoutFlags(amber, AbsoluteLayoutFlags.All);
+            abs.Children.Add(amber);
+            Content = abs;
+        }
+    }
+}
